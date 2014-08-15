785b502272a38927747bc44dd48405c997695d81
Anwar Choukah <anwarchoukah@gmail.com>
2014-08-15 10:58:07 +0100

    More additions and cleanup
    * added icomoon fonts
    * added google fonts to header.php
    * added viewport=initial-width to header.php
    * added styles and media queries

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -6,15 +6,21 @@
 
   <title><?php echo html($site->title()) ?> | <?php echo html($page->title()) ?></title>
   <meta name="description" content="<?php echo html($site->description()) ?>" />
+  <meta name="robots" content="index, follow" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <?php
 
   echo css(array(
-    'http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,400italic',
-    'assets/css/main.css',
+    'http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic|Playfair+Display:400,700,900,400italic,700italic,900italic|Inconsolata',
   ));
 
   ?>
+
+  <?php echo css('assets/css/main.css') ?>
+  <?php echo js('assets/js/jquery-1.11.0.min.js') ?>
+  <?php echo js('assets/js/lightbox.min.js') ?>
+  <?php echo js('//www.google-analytics.com/analytics.js') ?>
 
 </head>
 <body>
